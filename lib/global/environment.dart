84734bada18d5d351ebfe04ea9dd964f0c5e19ba
Juan Rodriguez <nyxjuan@hotmail.com>
2021-07-06 18:44:34 -0500

    Rutas heroku actualizadas

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 
 class Environment {
   static String apiUrl = Platform.isAndroid
-      ? 'http://10.0.2.2:3000/api'
-      : 'http://localhost:3000/api';
-  static String socketUrl =
-      Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+      ? 'https://chat-node-laluz.herokuapp.com/api'
+      : 'https://chat-node-laluz.herokuapp.com/api';
+  static String socketUrl = Platform.isAndroid
+      ? 'https://chat-node-laluz.herokuapp.com'
+      : 'https://chat-node-laluz.herokuapp.com';
 }
